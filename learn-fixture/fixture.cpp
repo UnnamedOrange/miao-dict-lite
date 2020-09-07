3e5abd516340f2cd9e733bab6f7d5b907db08c15
UnnamedOrange <lycheng1215@sina.com>
2020-09-07 22:12:33 +0800

    修改文件名；开始编写 fixture。

--- learn-fixture/fixture.cpp
+++ learn-fixture/fixture.cpp
@@ -2,10 +2,11 @@
 #undef min
 #undef max
 #include <windowsx.h>
-#include <utils/window.hpp>
+#include "utils/window.hpp"
 
-class flash_window : public window
+class fixture_window : public window
 {
+	static constexpr int cx = 500;
 	virtual INT_PTR WindowProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) override
 	{
 		switch (message)
@@ -20,7 +21,14 @@ class flash_window : public window
 	}
 	BOOL OnCreate(HWND hwnd, LPCREATESTRUCT lpCreateStruct)
 	{
-		caption(L"flash");
+		caption(L"fixture");
+		SetWindowLongW(hwnd, GWL_STYLE,
+			WS_CLIPCHILDREN | WS_CLIPSIBLINGS | WS_POPUP | WS_SYSMENU);
+		SetWindowLongW(hwnd, GWL_EXSTYLE, WS_EX_LAYERED);
+		SetLayeredWindowAttributes(hwnd, NULL, 127, LWA_ALPHA);
+		width(300);
+		height(200);
+
 		return TRUE;
 	}
 	void OnDestroy(HWND hwnd)
@@ -29,7 +37,7 @@ class flash_window : public window
 	}
 
 public:
-	flash_window()
+	fixture_window()
 	{
 
 	}
@@ -37,7 +45,8 @@ public:
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE, _In_ PWSTR, _In_ int)
 {
-	flash_window main_window;
+	SetProcessDpiAwarenessContext(DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2);
+	fixture_window main_window;
 	main_window.create();
 	return main_window.message_loop();
 }
---
+++ learn-flash/flash.cpp
@@ -0,0 +1,43 @@
+﻿#include <Windows.h>
+#undef min
+#undef max
+#include <windowsx.h>
+#include "utils/window.hpp"
+
+class flash_window : public window
+{
+	virtual INT_PTR WindowProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) override
+	{
+		switch (message)
+		{
+			HANDLE_MSG(hwnd, WM_CREATE, OnCreate);
+			HANDLE_MSG(hwnd, WM_DESTROY, OnDestroy);
+
+		default:
+			return DefWindowProcW(hwnd, message, wParam, lParam);
+		}
+		return 0;
+	}
+	BOOL OnCreate(HWND hwnd, LPCREATESTRUCT lpCreateStruct)
+	{
+		caption(L"flash");
+		return TRUE;
+	}
+	void OnDestroy(HWND hwnd)
+	{
+		PostQuitMessage(0);
+	}
+
+public:
+	flash_window()
+	{
+
+	}
+};
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE, _In_ PWSTR, _In_ int)
+{
+	flash_window main_window;
+	main_window.create();
+	return main_window.message_loop();
+}
